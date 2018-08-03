454b0c19a067c52e3935a8313cd8adce73c83b44
Tuan <tuandht97@gmail.com>
2018-08-03 10:58:45 +0700

    Tạo CSDL user, node

--- database/migrations/2018_08_03_033236_create_nodes_table.php
+++ database/migrations/2018_08_03_033236_create_nodes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNodesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,21 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('nodes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('fullname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('avatar');
-            $table->tinyInteger('auth');
-            $table->boolean('sex');
+            $table->string('image');
+            $table->string('email');
             $table->string('phone',11);
+            $table->boolean('sex');
             $table->date('birthday');
+            $table->string('birthplace');
+            $table->boolean('is_alive');
             $table->string('address');
-            $table->rememberToken();
+            $table->date('death_date');
+            $table->text('cause_of_death');
+            $table->string('burial_place');
+            $table->text('biography');
             $table->timestamps();
         });
     }
@@ -36,6 +39,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('nodes');
     }
 }
